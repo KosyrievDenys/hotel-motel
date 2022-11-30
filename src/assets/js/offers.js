35d50293e9ead8719cdffc752c77ac094712bccc
denys_kosyriev <utross2241@gmail.com>
2022-11-30 16:13:11 +0200

    feature: section rooms

---
+++ src/assets/js/offers.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper-offers', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    type: 'fraction',
+  }
+})
